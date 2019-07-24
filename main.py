b22cc39589ed40372c6285d2c853f24401794cfa
Victor Zhou <vzhou842@gmail.com>
2019-07-23 23:32:02 -0400

    Finish sentiment classifier implementation

--- main.py
+++ main.py
@@ -2,16 +2,25 @@ import numpy as np
 import random
 
 from rnn import RNN
-from data import *
+from data import train_data, test_data
 
-words = list(set([w for text in train_data.keys() for w in text.split(' ')]))
-vocab_size = len(words)
+# Create the vocabulary.
+vocab = list(set([w for text in train_data.keys() for w in text.split(' ')]))
+vocab_size = len(vocab)
 print('%d unique words found' % vocab_size)
-word_to_idx = { w: i for i, w in enumerate(words) }
-idx_to_word = { i: w for i, w in enumerate(words) }
-print(word_to_idx)
+
+# Assign indices to each word.
+word_to_idx = { w: i for i, w in enumerate(vocab) }
+idx_to_word = { i: w for i, w in enumerate(vocab) }
+print(word_to_idx['good'])
+print(idx_to_word[0])
 
 def createInputs(text):
+  '''
+  Returns an array of one-hot vectors representing the words in the input text string.
+  - text is a string
+  - Each one-hot vector has shape (vocab_size, 1)
+  '''
   inputs = []
   for w in text.split(' '):
     v = np.zeros((vocab_size, 1))
@@ -19,46 +28,54 @@ def createInputs(text):
     inputs.append(v)
   return inputs
 
+def softmax(xs):
+  # Applies the Softmax Function to the input array.
+  return np.exp(xs) / sum(np.exp(xs))
+
+# Initialize our RNN!
 rnn = RNN(vocab_size, 2)
 
-# Training loop
-for epoch in range(1500):
-  train_loss = 0
+def processData(data, backprop=True):
+  '''
+  Returns the RNN's loss and accuracy for the given data.
+  - data is a dictionary mapping text to True or False.
+  - backprop determines if the backward phase should be run.
+  '''
+  items = list(data.items())
+  random.shuffle(items)
+
+  loss = 0
   num_correct = 0
 
-  items = list(train_data.items())
-  random.shuffle(items)
   for x, y in items:
     inputs = createInputs(x)
     target = int(y)
 
-    outs, _ = rnn.forward(inputs)
-    probs = np.exp(outs[-1]) / np.sum(np.exp(outs[-1]))
+    # Forward
+    out, _ = rnn.forward(inputs)
+    probs = softmax(out)
 
     # Calculate loss / accuracy
-    train_loss -= np.log(probs[target])
+    loss -= np.log(probs[target])
     num_correct += int(np.argmax(probs) == target)
 
-    # Build dL/d(outs)
-    d_L_d_outs = probs
-    d_L_d_outs[target] -= 1
+    if backprop:
+      # Build dL/dy
+      d_L_d_y = probs
+      d_L_d_y[target] -= 1
 
-    rnn.backprop(d_L_d_outs)
+      # Backward
+      rnn.backprop(d_L_d_y)
 
-  if epoch % 50 == 49:
+  return loss / len(data), num_correct / len(data)
+
+# Training loop
+for epoch in range(1000):
+  train_loss, train_acc = processData(train_data)
+
+  if epoch % 100 == 99:
     print('--- Epoch %d' % (epoch + 1))
-    print('Train:\tLoss %f | Accuracy: %d / %d' % (train_loss / len(train_data), num_correct, len(train_data)))
+    print('Train:\tLoss %.3f | Accuracy: %.3f' % (train_loss, train_acc))
 
-    test_loss = 0
-    test_num_correct = 0
-    for x, y in test_data.items():
-      inputs = createInputs(x)
-      target = int(y)
-
-      outs, _ = rnn.forward(inputs)
-      probs = np.exp(outs[-1]) / np.sum(np.exp(outs[-1]))
-
-      test_loss -= np.log(probs[target])
-      test_num_correct += int(np.argmax(probs) == target)
-
-    print('Test:\tLoss %f | Accuracy: %d / %d' % (test_loss / len(test_data), test_num_correct, len(test_data)))
+    test_loss, test_acc = processData(test_data, backprop=False)
+    print('Test:\tLoss %.3f | Accuracy: %.3f' % (test_loss, test_acc))
--- rnn.py
+++ rnn.py
@@ -1,54 +1,83 @@
 import numpy as np
+from numpy.random import randn
 
 class RNN:
+  # A many-to-one Vanilla Recurrent Neural Network.
+
   def __init__(self, input_size, output_size, hidden_size=64):
-    self.Whh = np.random.randn(hidden_size, hidden_size) / 1000
-    self.Wxh = np.random.randn(hidden_size, input_size) / 1000
-    self.Why = np.random.randn(output_size, hidden_size) / 1000
+    # Weights
+    self.Whh = randn(hidden_size, hidden_size) / 1000
+    self.Wxh = randn(hidden_size, input_size) / 1000
+    self.Why = randn(output_size, hidden_size) / 1000
+
+    # Biases
     self.bh = np.zeros((hidden_size, 1))
     self.by = np.zeros((output_size, 1))
 
   def forward(self, inputs):
-    self.last_inputs = inputs
-    self.last_hs = {}
-
+    '''
+    Perform a forward pass of the RNN using the given inputs.
+    Returns the final output and hidden state.
+    - inputs is an array of one hot vectors with shape (input_size, 1).
+    '''
     h = np.zeros((self.Whh.shape[0], 1))
-    self.last_hs[-1] = h
-    ys = []
 
+    self.last_inputs = inputs
+    self.last_hs = { 0: h }
+
+    # Perform each step of the RNN
     for i, x in enumerate(inputs):
       h = np.tanh(self.Wxh @ x + self.Whh @ h + self.bh)
-      y = self.Why @ h + self.by
+      self.last_hs[i + 1] = h
 
-      self.last_hs[i] = h
-      ys.append(y)
+    # Compute the output
+    y = self.Why @ h + self.by
 
-    return ys, h
+    return y, h
 
-  def backprop(self, d_out, learn_rate=1e-2):
-    num_timesteps = len(self.last_inputs)
+  def backprop(self, d_y, learn_rate=2e-2):
+    '''
+    Perform a backward pass of the RNN.
+    - d_y (dL/dy) has shape (output_size, 1).
+    - learn_rate is a float.
+    '''
+    n = len(self.last_inputs)
 
+    # Calculate dL/dWhy and dL/dby.
+    d_Why = d_y @ self.last_hs[n].T
+    d_by = d_y
+
+    # Initialize dL/dWhh, dL/dWxh, and dL/dbh to zero.
     d_Whh = np.zeros(self.Whh.shape)
     d_Wxh = np.zeros(self.Wxh.shape)
     d_bh = np.zeros(self.bh.shape)
 
-    d_Why = d_out @ self.last_hs[num_timesteps - 1].T
-    d_by = d_out
+    # Calculate dL/dh for the last h.
+    # dL/dh = dL/dy * dy/dh
+    d_h = self.Why.T @ d_y
 
-    d_h = (self.Why.T @ d_out)
+    # Backpropagate through time.
+    for t in reversed(range(n)):
+      # An intermediate value: dL/dh * (1 - h^2)
+      temp = ((1 - self.last_hs[t + 1] ** 2) * d_h)
 
-    for t in reversed(range(num_timesteps)):
-      temp = ((1 - self.last_hs[t] ** 2) * d_h)
-
+      # dL/db = dL/dh * (1 - h^2)
       d_bh += temp
-      d_Whh += temp @ self.last_hs[t - 1].T
+
+      # dL/dWhh = dL/dh * (1 - h^2) * h_{t-1}
+      d_Whh += temp @ self.last_hs[t].T
+
+      # dL/dWxh = dL/dh * (1 - h^2) * x
       d_Wxh += temp @ self.last_inputs[t].T
 
+      # Next dL/dh = dL/dh * (1 - h^2) * Whh
       d_h = self.Whh @ temp
 
+    # Clip to prevent exploding gradients.
     for d in [d_Wxh, d_Whh, d_Why, d_bh, d_by]:
-      np.clip(d, -10, 10, out=d)
+      np.clip(d, -1, 1, out=d)
 
+    # Update weights and biases using gradient descent.
     self.Whh -= learn_rate * d_Whh
     self.Wxh -= learn_rate * d_Wxh
     self.Why -= learn_rate * d_Why
